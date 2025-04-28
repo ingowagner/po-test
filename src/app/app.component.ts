3c95de32baabcb8fbd492e51df5fcfe39125a17c
Ingo <ingo.wagner@totvs.com>
2025-04-28 08:14:35 -0400

    font issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PoFieldModule } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [PoFieldModule]
 })
 export class AppComponent {
   title = 'po-test';
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { ICONS_DICTIONARY, AnimaliaIconDictionary } from '@po-ui/ng-components';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    {
+      provide: ICONS_DICTIONARY,
+      useValue: AnimaliaIconDictionary,
+    }
+  ]
 };
